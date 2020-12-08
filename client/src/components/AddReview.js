bad1c355f21f9ca66ec8c44ac413773a3316c18f
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-12-07 19:26:37 -0500

    join tables done

--- client/src/components/AddReview.js
+++ client/src/components/AddReview.js
@@ -1,55 +1,99 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import { useParams,useLocation,useHistory } from "react-router-dom";
+import RestaurantFinder from '../apis/RestaurantFinder';
 const AddReview = () => {
-    const [name, setName] = useState("");
-    const [reviewText, setReviewText] = useState("");
-    const [rating, setRating] = useState("Rating");
-    
-    const handleNameChange = (event) => {
-        console.log(event.target.value)
-        setName(event.target.value)
+  const [name, setName] = useState("");
+  const [reviewText, setReviewText] = useState("");
+  const [rating, setRating] = useState("Rating");
+  const { id } = useParams();
+  const history=useHistory();
+  const location=useLocation();
+
+  console.log(id);
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    console.log(event.target.value);
+    setRating(event.target.value);
+  };
+
+  const handleReviewTextChange = (event) => {
+    console.log(event.target.value);
+    setReviewText(event.target.value);
+  };
+
+  const handleSubmitReview = async (e) => {
+    e.preventDefault();
+
+    try {
+        const newReview=await RestaurantFinder.post(`/${id}/addReview`,{
+            name:name,
+            review:reviewText,
+            rating:rating
+        })
+        history.push("/")
+        history.push(location.pathname)
+        console.log(newReview)
+    } catch (err) {
+      console.error(err.message);
     }
+  };
 
-    const handleRatingChange=(event)=>{
-        console.log(event.target.value)
-        setRating(event.target.value)
-    }
-
-    const handleReviewTextChange=(event)=>{
-        console.log(event.target.value)
-        setReviewText(event.target.value)
-    }
-
-    return (
-        <div className='mb-2'>
-            <form>
-                <div className='form-row'>
-                    <div className='form-group col-8'>
-                        <label htmlFor='name'>Name</label>
-                        <input className='form-control' id='name' type='text' placeholder='name' value={name} onChange={handleNameChange} />
-                    </div>
-                    <div className='form-group col-4'>
-                        <label htmlFor='rating'>Rating</label>
-                        <select id='rating' className='custom-select' value={rating} onChange={handleRatingChange}>
-                            <option disabled={true}>Rating</option>
-                            <option value='1'>1</option>
-                            <option value='2'>2</option>
-                            <option value='3'>3</option>
-                            <option value='4'>4</option>
-                            <option value='5'>5</option>
-                        </select>
-                    </div>
-
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='Review'>Review</label>
-                    <textarea id='Review' className='form-control' value={reviewText} onChange={handleReviewTextChange}></textarea>
-                </div>
-                <button type='submit' className='btn btn-primary'>Submit</button>
-            </form>
-
+  return (
+    <div className="mb-2">
+      <form>
+        <div className="form-row">
+          <div className="form-group col-8">
+            <label htmlFor="name">Name</label>
+            <input
+              className="form-control"
+              id="name"
+              type="text"
+              placeholder="name"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div className="form-group col-4">
+            <label htmlFor="rating">Rating</label>
+            <select
+              id="rating"
+              className="custom-select"
+              value={rating}
+              onChange={handleRatingChange}
+            >
+              <option disabled={true}>Rating</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
         </div>
-    )
-}
+        <div className="form-group">
+          <label htmlFor="Review">Review</label>
+          <textarea
+            id="Review"
+            className="form-control"
+            value={reviewText}
+            onChange={handleReviewTextChange}
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          onClick={handleSubmitReview}
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default AddReview
+export default AddReview;
--- server/server.js
+++ server/server.js
@@ -1,135 +1,143 @@
-require('dotenv').config();
-const morgan=require('morgan')
-const express=require('express');
-const db=require('./db')
-const cors=require('cors');
+require("dotenv").config();
+const morgan = require("morgan");
+const express = require("express");
+const db = require("./db");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // app.use((req,res,next)=>{
 //     console.log('something');
 //     next();
 // })
 
-
 // ROUTES
 // get all restaurants
-app.get('/api/v1/restaurants', async (req,res)=>{
-    try {
-        const allRestaurants=await db.query('SELECT * FROM restaurants');
-        console.log('route handler ran')
-        res.status(200).json({
-            'status':'success',
-            'results':allRestaurants.rows.length,
-            'data':{
-                'restaurants':allRestaurants.rows
-            }
-            
-        })
-    } catch (err) {
-        console.error(err.message)
-    }
-    
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const allRestaurants = await db.query("SELECT * FROM restaurants");
+    const restaurantRatingsData = await db.query(
+      "SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id,COUNT(*),TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id=reviews.restaurant_id"
+    );
+    console.log("allRestaurants",allRestaurants);
+    console.log("restaurantRatingsData",restaurantRatingsData);
+    res.status(200).json({
+      status: "success",
+      results: restaurantRatingsData.rows.length,
+      data: {
+        restaurants: restaurantRatingsData.rows,
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
- // Get a restaurant
- app.get('/api/v1/restaurants/:id',async (req,res)=>{
-     try {
-        const {id}=req.params;
-        const restaurant=await db.query("SELECT * FROM restaurants WHERE id=$1",[id]);
-        const reviews=await db.query("SELECT * FROM reviews WHERE restaurant_id=$1",[id]);
-        console.log(req.params)
-        res.status(200).json({
-            'status':'success',
-            'data':{
-                'restaurant':restaurant.rows[0],
-                'reviews':reviews.rows
-            }
-            
-        })
-     } catch (err) {
-         console.error(err.message)
-     }
-     
- })
+// Get a restaurant with reviews
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const restaurant = await db.query("SELECT * FROM restaurants WHERE id=$1", [
+      id,
+    ]);
+    const reviews = await db.query(
+      "SELECT * FROM reviews WHERE restaurant_id=$1",
+      [id]
+    );
+    console.log(req.params);
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: restaurant.rows[0],
+        reviews: reviews.rows,
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
- // add a restaurant
-app.post('/api/v1/restaurants', async (req,res)=>{
-    
-    try {
-        const {name,location,price_range}=req.body;
-const newRestaurant=await db.query("INSERT INTO restaurants (name,location,price_range) VALUES ($1,$2,$3) RETURNING *",[name,location,price_range])
+// add a restaurant
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const { name, location, price_range } = req.body;
+    const newRestaurant = await db.query(
+      "INSERT INTO restaurants (name,location,price_range) VALUES ($1,$2,$3) RETURNING *",
+      [name, location, price_range]
+    );
     // const createRestaurant=await ...
-    console.log(req.body)
+    console.log(req.body);
     res.status(201).json({
-        'status':'success',
-        'data':{
-            'restaurant':newRestaurant.rows[0]
-        }
-        
-    })
-    } catch (err) {
-        console.error(err.message)
-    }
-    
+      status: "success",
+      data: {
+        restaurant: newRestaurant.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //update a restaurant
-app.put('/api/v1/restaurants/:id',async (req,res)=>{
-    try{
-        const {id}=req.params;
-        const {name,location,price_range}=req.body;
-        const updatedRestaurant=await db.query("UPDATE restaurants SET name=$1,location=$2,price_range=$3 WHERE id=$4 RETURNING *",[name,location,price_range,id])
-        console.log(updatedRestaurant.rows[0])
-        res.status(200).json({
-            'status':'success',
-            'data':{
-                'restaurant':updatedRestaurant.rows[0]
-            }
-            
-        })
-    }catch(err){
-console.error(err.message)
-    }
-    
-})
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, location, price_range } = req.body;
+    const updatedRestaurant = await db.query(
+      "UPDATE restaurants SET name=$1,location=$2,price_range=$3 WHERE id=$4 RETURNING *",
+      [name, location, price_range, id]
+    );
+    console.log(updatedRestaurant.rows[0]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: updatedRestaurant.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // delete  restaurant
-app.delete('/api/v1/restaurants/:id',async (req,res)=>{
-    try {
-        const {id}=req.params;
-        const deletedRestaurant=await db.query("DELETE FROM restaurants WHERE id=$1",[id])
-        res.status(204).json({
-            'status':'success'           
-            
-        })
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedRestaurant = await db.query(
+      "DELETE FROM restaurants WHERE id=$1",
+      [id]
+    );
+    res.status(204).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-// // Get all reviews for a restaurant
-// app.get('/api/v1/restaurants/:id/reviews',async (req,res)=>{
-//     try {
-//         const {id}=req.params;
-//         const allReviews=await db.query('SELECT * FROM reviews WHERE restaurant_id=$1',[id]);
-//         console.log('route handler ran')
-//         res.status(200).json({
-//             'status':'success',
-//             'results':allReviews.rows.length,
-//             'data':{
-//                 'reviews':allReviews.rows
-//             }
-            
-//         })
-//     } catch (err) {
-//         console.error(err.message)
-//     }
-    
-// })
+// Add a review to restaurant
+app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { restaurant_id, name, review, rating } = req.body;
+    const newReview = await db.query(
+      "INSERT INTO reviews (restaurant_id,name, review,rating) VALUES ($1,$2,$3,$4) RETURNING *",
+      [id, name, review, rating]
+    );
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`server is listening to PORT ${PORT}`)
-})
+    res.status(201).json({
+      status: "success",
+      data: {
+        review: newReview.rows[0],
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is listening to PORT ${PORT}`);
+});
